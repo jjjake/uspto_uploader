ff8c373bc0bda415ef4fc6abb513ef87f6212ae7
Alex Buie <abuie@archive.org>
2014-05-01 11:38:28 -0700

    Add metadata from patentapplications.csv. Simplified identifiers ('drop 0XX- prefix'). Updated description field (links to OCRd PDF & streaming, not original PDF).

--- uspto_uploader.py
+++ uspto_uploader.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import pathlib
 import sys
+import csv
 
 from internetarchive import get_item, log
 
@@ -27,31 +28,50 @@ def item_generator():
 
 # get_metadata()
 # ________________________________________________________________________________________
+def get_csv_metadata(identifier):
+    f = open('/Volumes/patent/patentapplications.csv') 
+    csv_md = {}
+    for md in csv.DictReader(f):
+        if md.get('folder') == identifier:
+            csv_md['firstnamedinventor'] = md.get('firstnamedinventor')
+            csv_md['groupartunit'] = md.get('groupartunit') 
+            csv_md['class'] = md.get('class')
+            csv_md['subclass'] = md.get('subclass')
+            return csv_md
+
+
+# get_metadata()
+# ________________________________________________________________________________________
 def get_metadata(item, item_dir):
+    csv_md = get_csv_metadata(item.identifier)
     pdfs = list(item_dir.glob('*pdf'))
-    html = ('United States Patent and Trademark Office documents.<br /><br />Click the '
-            'links below to access the documents in this item:<br />')
+    html = ('<b>Click the links below to access the documents in this item:</b>'
+            '<br /><br />')
     for pdf in pdfs:
         url = '//archive.org/download/{i}/{f}'.format(i=item.identifier, f=pdf.name)
-        html += '<a href="{u}">{f}</a><br />'.format(u=url, f=pdf.name)
+        html += ('{f} <a href="//archive.org/download/{id}/{f}_text.pdf">pdf</a>'
+                 ' <a href="//archive.org/stream/{id}/{f}">stream</a>'
+                 '<br />'.format(f=pdf.stem, id=item.identifier))
     _title = item.identifier.split('gov.')[-1].replace('.', ' ')
     title = _title.title().replace('Uspto', 'USPTO')
     md = dict(
         collection='uspto',
         mediatype='texts',
-        subject=['U.S. Patents', 'U.S. Trademarks'],
+        contributor='Think Computer Foundation',
+        creator='United States Patent and Trademark Office',
+        subject=['U.S. Patents'],
         language='eng',
         description=html,
         title=title,
     )
-    return md
+    return dict((k, v) for (k, v) in md.items() if v)
 
 
 # main()
 # ________________________________________________________________________________________
 if __name__ == '__main__':
-    for i in item_generator():
-        identifier = '-'.join(str(i).split('/')[3:])
+    for _i, i in enumerate(item_generator()):
+        identifier = str(i).split('/')[-1]
 
         config = {'logging': {'level': 'INFO'}}
         item = get_item(identifier, config=config)
@@ -62,4 +82,3 @@ if __name__ == '__main__':
         item_dir = '{d}/'.format(d=str(i))
         md = get_metadata(item, i)
         resps = item.upload(item_dir, metadata=md, verbose=True, checksum=True)
-        sys.exit()
